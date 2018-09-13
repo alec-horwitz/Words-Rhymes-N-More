e77d7743bc2e060f59fad9956332f57526562208
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-09-13 10:36:40 -0400

    Used the recource generator to init a query model, controller, routes, and migration

---
+++ db/migrate/20180913143429_create_queries.rb
@@ -0,0 +1,10 @@
+class CreateQueries < ActiveRecord::Migration[5.1]
+  def change
+    create_table :queries do |t|
+      t.string :word_list
+      t.string :action
+
+      t.timestamps
+    end
+  end
+end
